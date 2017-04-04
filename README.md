7569713f794ccbba46fb73f198f13b669823d741
Emil Säll <emilsall@gmail.com>
2017-04-03 22:31:25 -0500

    Docker conf, npm stuff and basic theme added

---
+++ README.md
@@ -0,0 +1,40 @@
+# Custom Wordpress theme starter
+with Docker server and npm/yarn build scripts
+
+## Gitted by
+Emil Säll
+emil@sallmedia.se
+http://sallmedia.se
+
+## Theme
+The included theme is a slightly dieted version of [http://underscores.me/](_s) just to have something to start with.
+
+---
+
+## Docker
+
+Runs webserver on localhost:8080 and mysql (mariadb) on localhost:3886
+
+### Start docker
+`docker-compose up`
+
+---
+
+## Build tools
+
+### To install:
+
+cd to the theme folder
+
+`yarn` (or `npm install`)
+
+### To build:
+`yarn run build` (or `npm run build`)
+
+### To develop:
+`yarn run watch` (or `npm run watch`)
+
+---
+
+Enjoy!
+
---
+++ wp-content/themes/sallmedia/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package sallmedia_2017
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sallmedia' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'sallmedia' ), 'WordPress' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
